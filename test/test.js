cd9d5d20426bd47fb152cb6176f55d44929db861
Max Ogden <mogden@gmail.com>
2012-08-02 19:54:51 -0700

    fix var tpo

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var mdb = require('../')
 
-var fruit = mdb('fruit.mdb').tables(function(err, tables) {
+var fruit = mdb('fruit.mdb')
+
+fruit.tables(function(err, tables) {
   tables.forEach(function(table) {
     fruit.toCSV(table, function(err, csv) {
       console.log(table, csv)
